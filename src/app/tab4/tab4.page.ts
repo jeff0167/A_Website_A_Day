afab24e2c10df4b4e7e7b7b6b622012f38677ac5
jeff0167 <73894404+jeff0167@users.noreply.github.com>
2023-03-13 13:50:36 +0100

    tab 4 update

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
+  templateUrl: './tab4.page3.html',
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
