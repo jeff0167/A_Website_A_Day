ddf0bafe27a02ea9178b56f5bfa66a3cbd554cc5
jefferen <jeff0167@edu.zealand.dk>
2023-03-16 21:21:05 +0100

    Pen and manga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'tab7',
+    loadChildren: () => import('./tab7/tab7.module').then( m => m.Tab7PageModule)
   },
+  {
+    path: 'tab8',
+    loadChildren: () => import('./tab8/tab8.module').then( m => m.Tab8PageModule)
+  },
+
 
 ];
 @NgModule({
